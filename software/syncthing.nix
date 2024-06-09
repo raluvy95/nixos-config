df4d352edd568399e5bc7f27316dd09f3710d1c0
raluvy95 <ralucadoge069@gmail.com>
2024-06-09 12:17:50 +0300

    updated syncthing id

--- software/syncthing.nix
+++ software/syncthing.nix
@@ -1,41 +1,50 @@
 { config, pkgs, ... }:
 let 
     userName = "cat";
+    userHome = "/home/${userName}/";
 in
 {
-  services.syncthing = {
-    # why did you put x = x + 1 whitespaces here lol
-    enable = true;
-    user = "${userName}";
-    dataDir = "/home/${userName}/Sync";    # Default folder for new synced folders
-    overrideDevices = true;
-    overrideFolders = true;
-  };
-
-  services.syncthing.settings = {
-    devices = {
-      # CNB devices
-      "my phone" = { id = "3QNJ7V5-3QCDPU6-UGAWXMT-MC6IKD3-KGY2MA4-QRTVMGJ-U466277-CE3KYQW"; };
-      # NRD devices
-      "nrd pc" = { id = "EVXBMLZ-V4KV35Z-ODXJ2VM-EZWYC3N-FYE7CX5-4MDNA7C-XDY2C7I-75TPYQ2"; };
-      "nrd tablet" = { id = "6IIPECD-WCB2GM6-QQBUX2O-6TJA6DJ-STNZRA2-43DEXS5-SYX3BRC-Z3HEDAD"; }; 
-      "nrd panther" = { id = "DAYZPX7-7LVYBZN-Q6ZUNA2-BJ2E7ED-O6MRTCY-SHNTL6K-DKDUPKC-DQIRBQP"; };
-      "nrd spes" = { id = "DRBCWGK-IEMEJUT-7UDIOKI-L43OVYB-ABUZJZW-25ITEDF-H3QM6D2-4OLNZA7"; };
+    services.syncthing = {
+        enable = true;
+        user = "${userName}";
+        dataDir = "${userHome}Sync";    # Default folder for new synced folders
     };
 
-    folders = {
-      # <3
-      "anax kulup" = {
-        id = "ejzvn-jtuie";
-        path = "/home/${userName}/Sync/anax kulup";
-        devices = [ "nrd pc" "nrd spes" "my phone" "nrd tablet" "nrd panther"];
-      };
-      # can't forget about ixni
-      "obsidian ixni" = {
-        id = "dlebo-khhal";
-        path = "/home/${userName}/Sync/obsidian::ixni";
-        devices = [ "nrd panther" "nrd spes" "my phone" "nrd tablet" "nrd pc" ];
-      };
+    services.syncthing.settings = {
+        devices = {
+
+            # CNB devices
+            "my phone" = { id = "3QNJ7V5-3QCDPU6-UGAWXMT-MC6IKD3-KGY2MA4-QRTVMGJ-U466277-CE3KYQW"; };
+
+            # NRD devices
+            "nrd pc" = { id = "CNM2TPL-H2W3DYZ-FUB54AT-AW464D6-U4236CO-CIL6IG4-Y4H6ZET-I67PLAF"; };
+            "nrd tablet" = { id = "6IIPECD-WCB2GM6-QQBUX2O-6TJA6DJ-STNZRA2-43DEXS5-SYX3BRC-Z3HEDAD"; }; 
+            "nrd panther" = { id = "36GHUBP-MBDDHYH-JH7D4JX-47AIA3E-KA6QJFL-XOO2DUM-H7XE33I-PE5J5QQ"; };
+            "nrd spes" = { id = "DRBCWGK-IEMEJUT-7UDIOKI-L43OVYB-ABUZJZW-25ITEDF-H3QM6D2-4OLNZA7"; };
+            "nrd macbook" = { id = "DTHQ6HZ-RZ76DTX-ANB2SLE-KQ6QQEO-CGHX56G-32M3MB7-3RBZ5RX-XKTZYQ5"; };
+            
+        };
+
+        folders = {
+            # <3
+            "anax kulup" = {
+                id = "ejzvn-jtuie";
+                path = "${userHome}/Sync/anax kulup";
+                devices = [ "nrd pc" "nrd spes" "my phone" "nrd tablet" "nrd panther"];
+            };
+
+            # can't forget about ixni
+            "obsidian ixni" = {
+                id = "dlebo-khhal";
+                path = "${userHome}/Sync/obsidian::ixni";
+                devices = [ "nrd panther" "nrd spes" "my phone" "nrd tablet" "nrd pc" ];
+            };
+
+            "obsidian kulup.mp3" = {
+                id = "bytx8-6vgq8";
+                path = "${userHome}/Sync/obsidian::kulupmp3";
+                devices = [ "nrd panther" "nrd spes" "my phone" "nrd tablet" "nrd pc" ];
+            };
+        };
     };
-  };
 }
