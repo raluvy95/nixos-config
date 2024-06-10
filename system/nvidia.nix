db32ef572980d5f8414a44e5ff0d420373555b0b
raluvy95 <ralucadoge069@gmail.com>
2024-06-10 17:59:08 +0300

    FUCK YOU NVIDIA

--- system/nvidia.nix
+++ system/nvidia.nix
@@ -39,8 +39,21 @@
 	  # accessible via `nvidia-settings`.
     nvidiaSettings = false;
 
-    # This installs 550
-    package = config.boot.kernelPackages.nvidiaPackages.stable; 
+    # This installs 535
+    # Nvidia 550 causes kernel panic (on shutdown, during upgrade
+    # or even rare action such as closing telegram client) for my machine
+    # Related issue: https://forums.developer.nvidia.com/t/series-550-freezes-laptop/284772
+    # It is recommend to keep 535 if you have laptop gaming with Nvidia RTX
+
+    # FUCK YOU NVIDIA  
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "535.154.05";
+      sha256_64bit = "sha256-fpUGXKprgt6SYRDxSCemGXLrEsIA6GOinp+0eGbqqJg=";
+      sha256_aarch64 = "sha256-G0/GiObf/BZMkzzET8HQjdIcvCSqB1uhsinro2HLK9k=";
+      openSha256 = "sha256-wvRdHguGLxS0mR06P5Qi++pDJBCF8pJ8hr4T8O6TJIo=";
+      settingsSha256 = "sha256-9wqoDEWY4I7weWW05F4igj1Gj9wjHsREFMztfEmqm10=";
+      persistencedSha256 = "sha256-d0Q3Lk80JqkS1B54Mahu2yY/WocOqFFbZVBh+ToGhaE=";
+    }; 
 
     # Remove this if you have desktop pc and don't have intel igpu
     prime = {
